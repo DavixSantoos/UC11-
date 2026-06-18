15ccd909683f3349f4765ab44803d6df107bc345
David C Santos <133790186+DavixSantoos@users.noreply.github.com>
2026-06-18 20:18:06 -0300

    Estruturas

---
+++ UtilitarioApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using UtilitarioApp;
+
+Calculadora c1 = new Calculadora();
+
+var resultado = c1.Somar(14, 17);
+var resultado2 = c1.Multiplicar(14, 17);
+
+Console.WriteLine($" O Resultado da soma é {resultado}");
+Console.WriteLine($" O Resultado da soma é {resultado2}");
